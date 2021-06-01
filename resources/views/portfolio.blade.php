a4251817632aedab487d6882cacb5506c992be2e
Carlos Guzman <alberto.guzman.220793@gmail.com>
2021-05-31 22:12:19 -0500

    add layout blade

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -1,1 +1,18 @@
-portfolio
+@extends('layout')
+
+@section('title', 'Portfolio')
+
+@section('content')
+    <h1>Portfolio</h1>
+
+    <ul>    
+            @forelse ($portfolio as $portfolioitem)
+                <li>{{  $portfolioitem['title'] }}</li>
+               
+                @empty
+               
+             <li>no hay proyectos ahora...</li>
+            @endforelse
+    </ul>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,14 @@
 |
 */
 
+$portfolio = [
+   /*  ['title' => 'Proyecto 1'],
+    ['title' => 'Proyecto 2'],
+    ['title' => 'Proyecto 3'],
+    ['title' => 'Proyecto 4'], */
+];
+
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
-Route::view('/portfolio', 'portfolio')->name('portfolio');
+Route::view('/portfolio', 'portfolio', compact('portfolio'))->name('portfolio');
 Route::view('/contact', 'contact')->name('contact');
